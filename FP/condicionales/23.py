a101bcc29fa1b21c2d585106ee93edaecee0c993
= <=>
2023-10-20 09:39:58 -0500

    py

--- FP/condicionales/23.py
+++ FP/condicionales/23.py
@@ -1,3 +1,17 @@
 import os
-os.system("cls)")
+os.system("cls")
 
+notamate = int(input("Notas en Matemáticas: "))
+notafisica = int(input("Notas en Física: "))
+
+if notamate > 17 : propina1 = 3 * notamate
+else : propina1 = 1 * notamate
+
+if notafisica > 15 : propina2 = 2 * notafisica
+else : propina2 = 0.5 * notafisica
+
+if (notafisica + notamate)/2 > 16 : obsequio = "Reloj"
+else : obsequio = "Nada"
+
+print(f"Propina:    s/{propina1+propina2:.2f}")
+print(f"Obsequio:   {obsequio}")
